f75bc5da15ca70592aea1d3728555e46ec46f63d
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-21 14:10:20 -0800

    Created create-react-app and added a pallete

---
+++ colors-app/src/App.js
@@ -0,0 +1,15 @@
+import Pallete from "./Palette.js";
+import {seedColors} from "./seedColors.js";
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Pallete {...seedColors[4]}/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ colors-app/src/Palette.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Pallete extends Component {
+  render() {
+    return <div className="Pallete">
+        {/* Navbar goes here */}
+        <div className="Pallete-colors">
+            {/* {color boxes} */}
+        </div>
+        {/* {footer} */}
+    </div>;
+  }
+}
