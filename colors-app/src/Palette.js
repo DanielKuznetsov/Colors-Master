c52d0096e3e3680ae03ee78625d23faec4563b28
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-21 20:20:04 -0800

    Styled the left side of the navbar

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./Color-box.js";
-import Slider from "rc-slider"; 
-import "rc-slider/assets/index.css";
 import "./Pallete.scss";
+import Navbar from "./Navbar.js";
 
 export default class Pallete extends Component {
   constructor(props) {
@@ -24,15 +23,7 @@ export default class Pallete extends Component {
 
     return (
       <div className="Pallete">
-        <div className="Slider">
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
+        <Navbar level={level} changeLevel={this.changeLevel} />
         {/* Navbar goes here */}
         <div className="Pallete-colors">{colorBoxes}</div>
         {/* {footer} */}
