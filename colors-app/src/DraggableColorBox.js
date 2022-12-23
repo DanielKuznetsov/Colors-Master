f0ef11faa2753fa6b7a8b36e9ce4598c6737c6c0
Daniel Kuznetsov <danilgkuznet@icloud.com>
2022-12-22 22:29:58 -0800

    Add DraggableColorBox

---
+++ colors-app/src/DraggableColorBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./DraggableColorBox.scss";
+
+export default class DraggableColorBox extends Component {
+  render() {
+    return (
+      <div
+        className="DraggableColorBox"
+        style={{ backgroundColor: this.props.color }}
+      >
+        {this.props.color}
+      </div>
+    );
+  }
+}
--- colors-app/src/NewPaletteForm.js
+++ colors-app/src/NewPaletteForm.js
@@ -12,13 +12,15 @@ import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { ChromePicker } from "react-color";
 import { Button } from "@mui/material";
+import DraggableColorBox from "./DraggableColorBox";
 
 const drawerWidth = 400;
 
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
   ({ theme, open }) => ({
     flexGrow: 1,
-    padding: theme.spacing(3),
+    height: `calc(100vh - 64px)`,
+    padding: theme.spacing(0),
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
@@ -143,11 +145,9 @@ export default function NewPaletteForm() {
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
-        <ul>
-          {colors.map((color) => (
-            <li style={{ backgroundColor: color }}>{color}</li>
-          ))}
-        </ul>
+        {colors.map((color, i) => (
+          <DraggableColorBox key={i} color={color} />
+        ))}
       </Main>
     </Box>
   );
